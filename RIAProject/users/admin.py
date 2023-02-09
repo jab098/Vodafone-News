60557cad35e4c9a83b65208b274e055d259d265f
Mattie2 <56387948+Mattie2@users.noreply.github.com>
2023-02-09 19:18:22 +0000

    users can submit write request

--- RIAProject/users/admin.py
+++ RIAProject/users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import WriteRequest
 
-# Register your models here.
+#this displays the admin panel with more detail
+class WriteRequestAdmin(admin.ModelAdmin):
+    
+    #displays all properties from Post tuple for each post
+    list_display = ('id', 'requestee', 'created_on', 'department', 'description', 'status')
+    
+    #the list_filter method filters the posts based on their status (submitted, accepted or rejected)
+    list_filter = ("status",)
+    
+    #these are the attributes that will be used to allow articles to be searched for (i.e. can search by title or content)
+    search_fields = ['id', 'requestee']
+
+admin.site.register(WriteRequest, WriteRequestAdmin)
--- WriteRequest/forms.py
+++ WriteRequest/forms.py
@@ -1,22 +1,6 @@
 from django import forms
-from django.contrib import messages
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 
 from .models import WriteRequest
-
-class UserRegisterForm(UserCreationForm):
-    # create a form that inherits from the standard usercreationform, so we can save extra fields including email address,first name and last name
-    email = forms.EmailField()
-    first_name = forms.CharField(max_length=30)
-    last_name = forms.CharField(max_length=30)
-
-    class Meta:
-        # affecting the user model
-        model = User
-        # specifies the fields that will appear in the form, and the order in which they'll appear
-        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2', ]
-
 
 class WriterRequestForm(forms.ModelForm):
     """
