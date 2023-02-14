0891f15d7dae08f2684fb2c165b50c2db5556666
Mattie2 <56387948+Mattie2@users.noreply.github.com>
2023-02-14 19:13:40 +0000

    removed unneccesarry lines

--- RIAProject/users/views.py
+++ RIAProject/users/views.py
@@ -1,9 +1,6 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import Group
-from django.contrib.auth import logout
-from django.http import HttpRequest
-from django.views.generic.base import TemplateView
 
 from .forms import UserRegisterForm, WriterRequestForm
 
