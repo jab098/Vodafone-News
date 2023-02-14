9f09f97ef826bbd5b746e47057c743a202273659
Mattie2 <56387948+Mattie2@users.noreply.github.com>
2023-02-14 19:07:57 +0000

    header refactor

--- RIAProject/users/urls.py
+++ RIAProject/users/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
-    path('request-write/', views.requestWriterPermissions)
+    path('request-write/', views.requestWriterPermissions, name='request-writer')
 ]
