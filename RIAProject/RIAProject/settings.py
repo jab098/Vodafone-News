88001ab72377761cd0216832f68751997b978732
Afreen Hussain <afreen-hussain@hotmail.co.uk>
2023-02-18 15:47:16 +0000

    Offline Functionality

--- RIAProject/RIAProject/settings.py
+++ RIAProject/RIAProject/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'VodaNews',
     'users',
-    'pwa'
+    'pwa',
 ]
 
 MIDDLEWARE = [
@@ -156,22 +156,22 @@ PWA_APP_ORIENTATION = 'any'
 PWA_APP_START_URL = '/'
 PWA_APP_STATUS_BAR_COLOR = 'default'
 PWA_APP_ICONS = [
-	{
-		'src': 'static/images/icon-160x160.png',
-		'sizes': '160x160'
-	}
+    {
+        'src': 'static/images/icon-160x160.png',
+        'sizes': '160x160'
+    }
 ]
 PWA_APP_ICONS_APPLE = [
-	{
-		'src': 'static/images/icon-160x160.png',
-		'sizes': '160x160'
-	}
+    {
+        'src': 'static/images/icon-160x160.png',
+        'sizes': '160x160'
+    }
 ]
 PWA_APP_SPLASH_SCREEN = [
-	{
-		'src': 'static/images/icon.png',
-		'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
-	}
+    {
+        'src': 'static/images/icon.png',
+        'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
+    }
 ]
 PWA_APP_DIR = 'ltr'
 PWA_APP_LANG = 'en-US'
--- RIAProject/static/js/serviceworker.js
+++ RIAProject/static/js/serviceworker.js
@@ -1,26 +1,44 @@
-var staticCacheName = 'djangopwa-v1';
+var staticCacheName = "django-pwa-v" + new Date().getTime();
+var filesToCache = [
+    '/offline',
+    '{% url "home" %}',
+    '/css/django-pwa-app.css',
+];
 
-self.addEventListener('install', function(event) {
-event.waitUntil(
-	caches.open(staticCacheName).then(function(cache) {
-	return cache.addAll([
-		'',
-	]);
-	})
-);
+// Cache on install
+self.addEventListener("install", event => {
+    this.skipWaiting();
+    event.waitUntil(
+        caches.open(staticCacheName)
+            .then(cache => {
+                return cache.addAll(filesToCache);
+            })
+    )
 });
 
-self.addEventListener('fetch', function(event) {
-var requestUrl = new URL(event.request.url);
-	if (requestUrl.origin === location.origin) {
-	if ((requestUrl.pathname === '/')) {
-		event.respondWith(caches.match(''));
-		return;
-	}
-	}
-	event.respondWith(
-	caches.match(event.request).then(function(response) {
-		return response || fetch(event.request);
-	})
-	);
+// Clear cache on activate
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(cacheName => (cacheName.startsWith("django-pwa-")))
+                    .filter(cacheName => (cacheName !== staticCacheName))
+                    .map(cacheName => caches.delete(cacheName))
+            );
+        })
+    );
+});
+
+// Serve from Cache
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+            .catch(() => {
+                return caches.match('offline');
+            })
+    )
 });
